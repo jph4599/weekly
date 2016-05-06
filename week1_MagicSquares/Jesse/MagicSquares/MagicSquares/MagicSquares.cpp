a4c477799efe978fc64b50da6598ac39d812d74b
Jesse Harvey <jph4599@gmail.com>
2016-05-05 22:17:53 -0700

    Testing how to commit

--- week1_MagicSquares/Jesse/MagicSquares/MagicSquares/MagicSquares.cpp
+++ week1_MagicSquares/Jesse/MagicSquares/MagicSquares/MagicSquares.cpp
@@ -5,7 +5,7 @@
 #include <iomanip>
 #include <iostream>
 using namespace std;
-
+// Test
 /*
 A 3x3 magic square is a 3x3 grid of the numbers 1-9 such that each row, column, and major diagonal adds up to 15. Here's an example:
 8 1 6
